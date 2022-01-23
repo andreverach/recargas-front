a6c19f86139d5310003d1377e8b11f11e35fd6ee
Andre Vera <andrevera.pe@gmail.com>
2022-01-23 00:52:07 -0500

    some fixes

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -25,22 +25,9 @@ export class AdminGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    //return true;//aqui depende si es tru o false dejara entrar a la ruta
+    return true;//aqui depende si es tru o false dejara entrar a la ruta
     //aqui mediante un pipe con map devolvemos tru o false depende de si el usuario esta conectado
-    return this.authService.hasUser().pipe(
-      map(user => user === null ? false : true),
-      tap(rpta => {//de esta manera tambien podemos redirigir usando el tap
-        if(!rpta)
-          this.router.navigate(['/admin/login']);
-      })
-      /*tap(user => console.log(user)),//esto se ejecutara pero no interrumpira el proceso
-      map(user => {
-        if(user === null){//si no hay usuario entonces redirecciona al inicio
-          return this.router.parseUrl('/');
-        }
-        return true;       
-      })*/
-    );
+    
   }
   
 }
